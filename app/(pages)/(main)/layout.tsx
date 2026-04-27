b295406d23295c19ce2fa1c346099cedad5e2fdc
nngtkhngoc <khanhngoc230705@gmail.com>
2026-04-27 14:03:14 +0700

    feat: click spark

--- app/(pages)/(main)/layout.tsx
+++ app/(pages)/(main)/layout.tsx
@@ -1,15 +1,20 @@
-import Header from "@/components/client/layout/Header";
-import Footer from "@/components/client/layout/Footer";
+import Header from '@/components/client/layout/Header';
+import Footer from '@/components/client/layout/Footer';
+import ClickSpark from '@/components/ClickSpark';
 
-export default function MainLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main className="py-[92px] px-[20px] lg:px-[160px]">{children}</main>
+      <ClickSpark
+        sparkColor="#665814"
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      >
+        <main className="py-[92px] px-[20px] lg:px-[160px]">{children}</main>
+      </ClickSpark>
       <Footer />
     </>
   );
